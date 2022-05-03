d43b5114e314b3900a06a605ab006b55fe9614e5
imranq2 <imranq2@hotmail.com>
2022-05-03 15:52:50 -0700

    show counter

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ class MyLogger(FhirLogger):
         """
         Handle messages at INFO level
         """
-        self._internal_logger.info(param)
+        # self._internal_logger.info(param)
+        pass
 
     def error(self, param: Any) -> None:
         """
@@ -36,8 +37,8 @@ class MyLogger(FhirLogger):
 
 class ResourceDownloader:
     def __init__(self) -> None:
-        # fhir_server = "fhir.icanbwell.com"
-        fhir_server = "fhir-next.icanbwell.com"
+        fhir_server = "fhir.icanbwell.com"
+        # fhir_server = "fhir-next.icanbwell.com"
         self.server_url = f"https://{fhir_server}/4_0_0"
         assert os.environ.get("FHIR_CLIENT_ID"), "FHIR_CLIENT_ID environment variable must be set"
         assert os.environ.get("FHIR_CLIENT_SECRET"), "FHIR_CLIENT_SECRET environment variable must be set"
@@ -59,8 +60,16 @@ class ResourceDownloader:
 
         output_file = await aiofiles.open('output.json', mode='w')
 
-        async def on_received_data(data: List[Dict[str, Any]], batch_number: Optional[int]) -> bool:
-            print(f"received batch: {batch_number}")
+        resource_count_holder = {
+            "resource_count": 0
+        }
+
+        async def on_received_data(resource_count_holder1: Dict[str, int], data: List[Dict[str, Any]],
+                                   batch_number: Optional[int]) -> bool:
+            # print(f"received batch: {batch_number}")
+            chunk_end_time = time.time()
+            resource_count_holder1["resource_count"] = resource_count_holder1["resource_count"]  + len(data)
+            print(f"[{resource_count_holder1['resource_count']}] {timedelta(seconds=chunk_end_time - start_job)}", end='\r')
             await output_file.write(json.dumps(data))
             await output_file.flush()
             return True
@@ -75,7 +84,7 @@ class ResourceDownloader:
             page_size_for_retrieving_ids=self.page_size_for_retrieving_ids,
             last_updated_start_date=self.start_date,
             last_updated_end_date=self.end_date,
-            fn_handle_batch=on_received_data
+            fn_handle_batch=lambda data, batch_number: on_received_data(resource_count_holder, data, batch_number)
         )
 
         end_job = time.time()
@@ -92,6 +101,7 @@ class ResourceDownloader:
         fhir_client = fhir_client.auth_scopes(self.auth_scopes)
         fhir_client = fhir_client.resource(self.resource)
         fhir_client = fhir_client.logger(MyLogger())
+        # fhir_client = fhir_client.additional_parameters(["_useAtlas=1"])
         return fhir_client
 
 
